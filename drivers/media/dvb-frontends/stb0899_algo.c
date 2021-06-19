13ddc3c4ae69ef74d90134e52d9ed9974bfadd07
Stricted <info@stricted.net>
2021-06-19 23:13:05 +0000

    media: dvb-frontends: Fix self assign warning
    Change-Id: Ie343bf04cdc1843395663d1ac5a09cd3a49a43ca

--- drivers/media/dvb-frontends/stb0899_algo.c
+++ drivers/media/dvb-frontends/stb0899_algo.c
@@ -926,7 +926,6 @@ static void stb0899_dvbs2_set_btr_loopbw(struct stb0899_state *state)
 		wn = (2 * (loopbw_percent * 1000) * 40 * zeta) /wn;  /*wn =wn 10^-8*/
 
 		k_indirect = (wn * wn) / K;
-		k_indirect = k_indirect;	  /*kindirect = kindirect 10^-6*/
 		k_direct   = (2 * wn * zeta) / K;	/*kDirect = kDirect 10^-2*/
 		k_direct  *= 100;
 
