73257d78627dc5df3b893d816ad9c7bf01cf45a6
Stricted <info@stricted.net>
2021-06-19 23:05:13 +0000

    amlogic: osd: Fix self assign warnings
    Change-Id: Ib7cbf268dcaf9115c423f5fea3eb88c5819aea24

--- drivers/amlogic/media/osd/osd_hw.c
+++ drivers/amlogic/media/osd/osd_hw.c
@@ -1857,8 +1857,7 @@ static u32 line_stride_calc(
 	/* need wr ddr is 32bytes aligned */
 	if (stride_align_32bytes)
 		line_stride = ((line_stride+1)>>1)<<1;
-	else
-		line_stride = line_stride;
+
 	return line_stride;
 }
 
@@ -1889,8 +1888,7 @@ static u32 line_stride_calc_afbc(
 	/* need wr ddr is 32bytes aligned */
 	if (stride_align_32bytes)
 		line_stride = ((line_stride+1) >> 1) << 1;
-	else
-		line_stride = line_stride;
+
 	return line_stride;
 }
 
