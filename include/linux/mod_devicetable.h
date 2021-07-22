4458bc168c4f1ba2186ac99a868db840be03910a
deadman96385 <seanhoyt963@gmail.com>
2021-07-21 19:19:50 -0500

    drivers: usb: Import Google's Type-C driver
    Change-Id: Ib224baf7f88334a07f1f1d106f4d0af36b59c72d

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -679,4 +679,16 @@ struct fsl_mc_device_id {
 };
 
 
+#define TYPEC_ANY_MODE	0x7
+/**
+ * struct typec_device_id - USB Type-C alternate mode identifiers
+ * @svid: Standard or Vendor ID
+ * @mode: Mode index
+ * @driver_data: Driver specific data
+ */
+struct typec_device_id {
+	__u16 svid;
+	__u8 mode;
+	kernel_ulong_t driver_data;
+};
 #endif /* LINUX_MOD_DEVICETABLE_H */
