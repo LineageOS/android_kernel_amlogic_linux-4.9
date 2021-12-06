ba30bcb848d14430365b7b0f8e1853c1f1290369
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-12-06 18:00:27 +0000

    usb: typec: Fix enum conversion warning
    Change-Id: Id61b8673915551bfa6ce81058bea066312b16345

--- drivers/usb/typec/tcpm.c
+++ drivers/usb/typec/tcpm.c
@@ -4638,7 +4638,7 @@ static int tcpm_psy_get_current_max(struct tcpm_port *port,
 	if (port->pps_data.active)
 		val->intval = port->pps_data.max_curr * 1000;
 	else
-		val->intval = max(tcpm_get_max_fixed_current(port),
+		val->intval = max_t(size_t, tcpm_get_max_fixed_current(port),
 				  port->current_limit) * 1000;
 
 	return 0;
