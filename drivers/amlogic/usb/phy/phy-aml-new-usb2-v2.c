2f801d95f22c2b66368fb22c47db97b51c31b5a6
jason <jason@khadas.com>
2022-03-14 10:49:07 +0000

    arm64: dts: Squashed kvim3 dts changes from vendor
    arm64: dts: meson: vim3: change power key to GPIOAO_7
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Ia9684cad5e3f903da5824aaf2383c593ad50d1dd
    
    arm64: dts: meson: vim3: define function key as HOME key
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I2e2021c56b22355f4320cd4eaad6e991f1256ffc
    
    arm64: dts: meson: vim3: change sysled gpio to GPIOAO_4
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I2ea9361427b127301d0ea60bee77bf2ef05f8339
    
    arm64: dts: meson: vim3: enable i2c_ao bus
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Ieb5aff49ec700bf2abd74df1c93098f3b21b9092
    
    arm64: dts: meson: vim3: set usb otg port as device only
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Iab0c5854f320a52b357ebd6914d6ec6eaae5a08b
    
    pcie: vim3: fixed the PCIE reset gpio issue
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Id29626a48a0b82d48c5aad6fdd45fc62f7a54135
    
    ethernet: vim3: add external ethernet support
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I9064985e47345fec6f099f4c847dbc8b7cf2189b
    
    i2c: vim3: disable i2c_0 bus
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I9a071e5f70f9100e3b0f8fbd2654e539c0ef3bb6
    
    ethernet: vim3: fix Gigabit ethernet issue
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Ie9cbda29f1a364381e345be9467fba4ba59e5a14
    
    i2c: vim3: remove unused i2c2 bus
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I5d7e29e2be659784eaa92e36fa7656e39f5bb709
    
    usb: vim3: default always enabel usb port power
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I3b796c98dbb83d03a83d88a406ec825180bdefcb
    
    arm64: dts: meson: vim3: change SPDIF out pin from GPIOAO_10 to GPIOH_4
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I2128685236f834d4d9003c472f93321c0b7d210b
    
    arm64: dts: meson: vim3: remove unuse model on i2c_3 bus
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I87607666c7af87ad2bdf4f7bbbc70ae3fbbfbbd4
    
    arm64: dts: meson: vim3: remove usable memory config
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Icf7ffbbf7e87070802e5d5f760f5ec7f8c2de8a8
    
    arm64: dts: meson: vim3: remove avout_mute-gpio
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I0ace8554df309d2d3e24341ddb299e0f0ff5b087
    
    arm64: dts: meson: vim3: disable alsaPORT-pdm
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Ib0a13b0d2798bdee878773a2b55956c4910ca5cf
    
    arm64: dts: meson: vim3: add SPDIF out support for PIN13
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I8dac2c7ba72925ead88dfaa3a754082bd4125d74
    
    arm64: dts: meson: vim3: fix dts file issue
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Ie701cd2b9cc787a5a843c05770f263cd6b297132
    
    arm64: dts: meson: vim3: add TS050 tp wakeup support
    
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I11db9bce7805247c39c0f326acbc1f7bf3295ebb
    
    DTS: Camera: add khadas imx415 dts support
    
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>
    
    Camera: Compatible with both OS08A10 and IMX415 [2/3]
    
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>
    
    VIM3：dts：enable uart_C
    
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>
    
    i2s add status
    
    Change-Id: Ib0e83231fb0d30e7d217f13aaa6f6cf8e2387475
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- drivers/amlogic/usb/phy/phy-aml-new-usb2-v2.c
+++ drivers/amlogic/usb/phy/phy-aml-new-usb2-v2.c
@@ -312,6 +312,8 @@ static int amlogic_new_usb2_probe(struct platform_device *pdev)
 	void __iomem	*phy_base;
 	void __iomem	*reset_base = NULL;
 	void __iomem	*phy_cfg_base[4] = {NULL, NULL, NULL, NULL};
+	const char *gpio_name = NULL;
+	struct gpio_desc *usb_gd = NULL;
 	int portnum = 0;
 	int phy_version = 0;
 	int reset_level = 0x84;
@@ -327,6 +329,14 @@ static int amlogic_new_usb2_probe(struct platform_device *pdev)
 	u32 phy_reset_level_bit[USB_PHY_MAX_NUMBER] = {-1};
 	u32 usb_reset_bit = 2;
 	char name[32];
+	gpio_name = of_get_property(dev->of_node, "gpio-vbus-power", NULL);
+	if (gpio_name) {
+		usb_gd = gpiod_get_index(&pdev->dev,
+					NULL, 0, GPIOD_OUT_LOW);
+		if (IS_ERR(usb_gd))
+			return -1;
+	gpiod_direction_output(usb_gd, 1);
+	}
 
 	prop = of_get_property(dev->of_node, "portnum", NULL);
 	if (prop)
